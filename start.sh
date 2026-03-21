9eb2c6bda2926eb4f606bcd2350ae4c67d215d21
nublado <nublado@nublado.com>
2026-03-20 18:45:00 -0700

    ...

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 python manage.py start_bots
-gunicorn config.asgi:application -w 1 -k uvicorn.workers.UvicornWorker
-
+gunicorn config.asgi:application \
+    -w 1 \
+    -k uvicorn.workers.UvicornWorker \
+    -b 0.0.0.0:$PORT \
+    --log-level info
