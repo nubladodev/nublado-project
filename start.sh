4a0a7ee75334a4e80d007f5f07449788125f1475
nublado <nublado@nublado.com>
2026-03-26 19:30:15 -0700

    ...

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ set -x  # show commands in logs
 echo "Setting webhooks..."
 python manage.py set_bot_webhooks
 
-echo "Starting gunicorn..."
+echo "Starting gunicorn with 2 workers..."
 exec gunicorn config.asgi:application \
-  -w 1 \
   -k uvicorn.workers.UvicornWorker \
-  -b 0.0.0.0:$PORT
+  -w 2 \
+  -b 0.0.0.0:$PORT \
+  --timeout 120
