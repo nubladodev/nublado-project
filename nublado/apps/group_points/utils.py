d82bd479bb943e3f67e37d46bb688cb5c8575a20
nublado <nublado@nublado.com>
2026-07-06 01:04:25 -0700

    ...

--- nublado/apps/group_points/utils.py
+++ nublado/apps/group_points/utils.py
@@ -1,6 +1,10 @@
 def extract_points(text: str, point_symbol: str, points_map: dict):
+    """
+    Determine the number of point symbols at the beginning of a string and return
+    the corresponding number of points.
+    """
     if not text:
         return None
 
-    count = len(text) - len(text.lstrip(point_symbol))
-    return points_map.get(count)
+    symbol_count = len(text) - len(text.lstrip(point_symbol))
+    return points_map.get(symbol_count)
